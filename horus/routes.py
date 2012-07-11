9c72fd109f1e4e278f9b444df5357e621e71e123
Sebastian Utz <su@rtme.net>
2012-07-11 17:08:23 +0200

    ``user_account_id`` should be used everywhere

--- horus/routes.py
+++ horus/routes.py
@@ -5,16 +5,16 @@ def includeme(config):
     config.add_route('horus_login', '/login')
     config.add_route('horus_logout', '/logout')
     config.add_route('horus_register', '/register')
-    config.add_route('horus_activate', '/activate/{user_id}/{code}', factory=UserAccountFactory)
+    config.add_route('horus_activate', '/activate/{user_account_id}/{code}', factory=UserAccountFactory)
     config.add_route('horus_forgot_password', '/forgot_password')
     config.add_route('horus_reset_password', '/reset_password/{code}')
-    config.add_route('horus_profile', '/profile/{user_id}', factory=UserAccountFactory,
-            traverse="/{user_id}")
-    config.add_route('horus_edit_profile', '/profile/{user_id}/edit',
-            factory=UserAccountFactory, traverse="/{user_id}")
+    config.add_route('horus_profile', '/profile/{user_account_id}', factory=UserAccountFactory,
+            traverse="/{user_account_id}")
+    config.add_route('horus_edit_profile', '/profile/{user_account_id}/edit',
+            factory=UserAccountFactory, traverse="/{user_account_id}")
 
     config.add_route('horus_admin_users_list', '/admin/users')
     config.add_route('horus_admin_users_create', '/admin/users/create')
-    config.add_route('horus_admin_users_edit', '/admin/users/{user_id}')
+    config.add_route('horus_admin_users_edit', '/admin/users/{user_account_id}')
 
     config.add_route('horus_velruse_callback', '/velruse_callback')
