00529eda6a783c18bf514a989f5f880cd166016f
Sebastian Utz <su@rtme.net>
2012-11-07 11:19:01 +0100

    version bump

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.0.13'
+      , version='0.0.14'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
