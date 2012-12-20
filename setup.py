18e034d77dc3365955e6caf7c0434658c65686b8
ovali <ovali@gmx.net>
2012-12-20 14:18:52 +0100

    raise to version 0.0.16

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.0.15'
+      , version='0.0.16'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
