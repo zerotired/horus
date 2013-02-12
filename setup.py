e15c9c08318c1efd7a4e88546cd1740a7bffcb5f
Sebastian Utz <su@rtme.net>
2013-02-12 10:29:39 +0100

    new version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.0.22'
+      , version='0.0.23'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
