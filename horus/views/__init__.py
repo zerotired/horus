6c1fbba44a080110777fde8ba6cc8d4fe6c574a1
Sebastian Utz <su@rtme.net>
2013-02-26 15:29:10 +0100

    again fixing last commits (grr…forgot how it worked ;)

--- horus/views/__init__.py
+++ horus/views/__init__.py
@@ -214,6 +214,12 @@ class AuthController(BaseController):
 
         if 'token' in self.request.POST:
             auth = openid_from_token(self.request.POST['token'], self.request)
+        elif self.request.context is not None and hasattr(self.request.context, 'profile'):
+            auth = {
+                'profile': self.request.context.profile,
+                'credentials': self.request.context.credentials,
+            }
+        if auth:
             auth_info = auth['profile']['accounts'][0]
             username = auth_info.get('username',
                                     auth['profile'].get('preferredUsername',
