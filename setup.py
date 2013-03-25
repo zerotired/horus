e29012a8978363170af78792ff4de185fcf5d103
ovali <ovali@gmx.net>
2013-03-25 16:42:41 +0100

    version bump 0.0.31

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.0.30'
+      , version='0.0.31'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
