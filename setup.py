8857bea027d7a7b214642481ff9caa3151d50ad1
andi <andi@itemfire.com>
2013-07-16 00:01:03 +0200

    - increase version to 0.0.34

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.0.33'
+      , version='0.0.34'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
